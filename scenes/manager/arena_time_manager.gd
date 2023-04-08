e55ab7530bc01ef7e0a254dc8239b6c100722ecc
Seth Brown <sethbrown.pe@gmail.com>
2023-04-07 22:11:18 -0500

    create victory screen

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,8 +1,18 @@
 extends Node
 class_name ArenaTimeManager
 
+@export var victory_screen_scene: PackedScene
 @onready var timer: Timer = $Timer
+
+
+func _ready() -> void:
+	timer.timeout.connect(on_timer_timeout)
 
 
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
+
+
+func on_timer_timeout():
+	var victory_screen_instance = victory_screen_scene.instantiate()
+	add_child(victory_screen_instance)
