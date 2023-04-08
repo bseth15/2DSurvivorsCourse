7d9a2551c4da2bfaa490544e2e04f5e21015b9e4
Seth Brown <sethbrown.pe@gmail.com>
2023-04-08 18:58:38 -0500

    animate enemy death

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -3,12 +3,17 @@ extends CharacterBody2D
 const MAX_SPEED: float = 40.0
 
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var visuals: Node2D = $Visuals
 
 
 func _process(delta: float) -> void:
 	var direction = get_direction_to_player()
 	velocity = direction * MAX_SPEED
 	move_and_slide()
+
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(-move_sign, 1)
 
 
 func get_direction_to_player():
