22895a9d1499b17b089f9515909407644e9bb779
Seth Brown <sethbrown.pe@gmail.com>
2023-04-07 20:19:16 -0500

    make upgrade functional

--- scenes/abilities/sword_ability_controller/sword_ability_controller.gd
+++ scenes/abilities/sword_ability_controller/sword_ability_controller.gd
@@ -3,12 +3,16 @@ extends Node
 const MAX_RANGE: float = 150.0
 
 @export var sword_ability: PackedScene
+
 var damage = 5
+var base_wait_time
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	base_wait_time = $Timer.wait_time
 	$Timer.timeout.connect(on_timer_timeout)
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 
 func on_timer_timeout():
@@ -39,3 +43,13 @@ func on_timer_timeout():
 
 	var enemy_direction = enemies[0].global_position - sword_instance.global_position
 	sword_instance.rotation = enemy_direction.angle()
+
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if upgrade.id != "sword_rate":
+		return
+
+	var percent_reduction = current_upgrades["sword_rate"]["quantity"] * 0.1
+	$Timer.wait_time = base_wait_time * (1 - percent_reduction)
+	$Timer.start()
+	print($Timer.wait_time)
