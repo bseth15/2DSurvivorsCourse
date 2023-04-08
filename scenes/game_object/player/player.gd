d7da701495f71745543330673fe37b200b686544
Seth Brown <sethbrown.pe@gmail.com>
2023-04-07 20:53:41 -0500

    add player health

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -3,15 +3,40 @@ extends CharacterBody2D
 const MAX_SPEED: float = 125.0
 const ACCELERATION_SMOOTHING: float = 25.0
 
+@onready var damage_interval_timer = $DamageIntervalTimer
 
-# Called when the node enters the scene tree for the first time.
+var number_colliding_bodies = 0
+
+
 func _ready() -> void:
-	pass # Replace with function body.
+	$CollisionArea2D.body_entered.connect(on_body_entered)
+	$CollisionArea2D.body_exited.connect(on_body_exited)
+	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	var target_velocity: Vector2 = direction * MAX_SPEED
 	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
 	move_and_slide()
+
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+	$HealthComponent.damage(1)
+	damage_interval_timer.start()
+	print($HealthComponent.current_health)
+
+
+func on_body_entered(other_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+
+
+func on_body_exited(other_body: Node2D):
+	number_colliding_bodies -= 1
+
+
+func on_damage_interval_timer_timeout():
+	check_deal_damage()
