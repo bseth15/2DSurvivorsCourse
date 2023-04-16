054d6563dc1327c79e5690f771126461064cb00a
Seth Brown <sethbrown.pe@gmail.com>
2023-04-16 18:26:39 -0500

    save and load meta progression data

--- scenes/autoload/meta_progression.gd
+++ scenes/autoload/meta_progression.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const SAVE_FILE_PATH = "user://game.sav"
+
 var save_data: Dictionary = {
 	"meta_upgrade_currency": 0,
 	"meta_upgrades": {}
@@ -8,6 +10,19 @@ var save_data: Dictionary = {
 
 func _ready() -> void:
 	GameEvents.experience_vial_collected.connect(on_experience_collected)
+	load_save_file()
+
+
+func load_save_file():
+	if not FileAccess.file_exists(SAVE_FILE_PATH):
+		return
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	save_data = file.get_var()
+
+
+func save():
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	file.store_var(save_data)
 
 
 func add_meta_upgrade(upgrade: MetaUpgrade):
