83a9b2710a544a8c657307ac6f58d15640adcc17
Seth Brown <sethbrown.pe@gmail.com>
2023-04-16 23:22:24 -0500

    fix spawn bug, end of main course

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -1,7 +1,7 @@
 extends Node
 
 # Just larger than half the view port width so that enemies spawn off-screen.
-const SPAWN_RADIUS = 375
+const SPAWN_RADIUS = 360
 
 @export var basic_enemy_scene: PackedScene
 @export var wizard_enemy_scene: PackedScene
@@ -29,13 +29,17 @@ func get_spawn_position() -> Vector2:
 	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
 	for i in 4:
 		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+		var additional_check_offset = random_direction * 20
 
-		var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1 << 0)
+		var query_parameters = PhysicsRayQueryParameters2D\
+			.create(player.global_position, spawn_position + additional_check_offset, 1 << 0)
+
 		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
 
 		if result.is_empty():
 			break
 		else:
+			spawn_position = Vector2.ZERO
 			random_direction = random_direction.rotated(deg_to_rad(90))
 
 	return spawn_position
@@ -48,12 +52,18 @@ func on_timer_timeout():
 	if player == null:
 		return
 
+	var spawn_position = get_spawn_position()
+	while spawn_position == Vector2.ZERO:
+		# Work around for spawning outside of map,
+		# will probably bite me in the ass some day.
+		spawn_position = get_spawn_position()
+
 	var enemy_scene = enemy_table.pick_item()
 	var enemy = enemy_scene.instantiate() as Node2D
 
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
-	enemy.global_position = get_spawn_position()
+	enemy.global_position = spawn_position
 
 
 func on_arena_difficulty_increased(arena_difficulty: int) -> void:
